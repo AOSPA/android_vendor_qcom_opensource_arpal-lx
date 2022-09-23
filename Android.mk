5c18209aea19dbfd6cd672f53a4668c06ac4ae0a
Alexander Koskovich <akoskovich@pm.me>
2022-09-23 14:07:49 -0700

    pal: Don't include any makefiles if BSP is not present
     * We are only picking up the split makefiles for convenience.
    
    Change-Id: I1573d332b0c57cc863965cc608f956d1c82a4166

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 ifneq ($(AUDIO_USE_STUB_HAL), true)
 ifeq ($(TARGET_USES_QCOM_MM_AUDIO), true)
+ifneq ($(wildcard vendor/qcom/proprietary/args),)
 
 LOCAL_PATH := $(call my-dir)
 PAL_BASE_PATH := $(call my-dir)
@@ -182,3 +183,4 @@ include $(PAL_BASE_PATH)/ipc/HwBinders/Android.mk
 
 endif #TARGET_USES_QCOM_MM_AUDIO
 endif #AUDIO_USE_STUB_HAL
+endif # ifneq ($(wildcard vendor/qcom/proprietary/args),)
