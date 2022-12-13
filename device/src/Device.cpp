31413d461a06264415b3011f233e4fbdf74a4a8f
Anver sadhique <quic_akurun@quicinc.com>
2022-12-13 06:16:19 -0800

    Revert "pal: device: force sampling freq to 48khz for WHS when speaker active"
    This reverts commit a232b10e75e135cd25a74e291706ff681b98c284
    
    Change-Id: Ia6924f606e5dc826bf9ec0042871bfeb2ecf56f4

--- device/src/Device.cpp
+++ device/src/Device.cpp
@@ -25,39 +25,6 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Changes from Qualcomm Innovation Center are provided under the following licens
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define LOG_TAG "PAL: Device"
@@ -407,13 +374,6 @@ int Device::open()
     devObj = Device::getInstance(&deviceAttr, rm);
 
     if (deviceCount == 0) {
-        PAL_INFO(LOG_TAG, "%s: is_multiple_sample_rate_combo_supported %d",__func__,rm->is_multiple_sample_rate_combo_supported);
-        if(!(rm->is_multiple_sample_rate_combo_supported) && (this->deviceAttr.id == PAL_DEVICE_OUT_WIRED_HEADSET)) {
-            int DeviceAcive = rm->isDeviceActive(PAL_DEVICE_OUT_SPEAKER);
-            PAL_ERR(LOG_TAG,"%s: Speaker DeviceAcive %d",__func__,DeviceAcive);
-            if(DeviceAcive == 1)
-                this->deviceAttr.config.sample_rate = 48000;
-        }
         std::string backEndName;
         rm->getBackendName(this->deviceAttr.id, backEndName);
         if (strlen(backEndName.c_str())) {
